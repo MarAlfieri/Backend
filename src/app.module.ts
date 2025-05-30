2ac35779c41124bf285e7d8ca04a6309772ff826
MarAlfieri <alfierisistemas@gmail.com>
2025-05-30 19:12:49 -0300

    Actualizar todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { TrackService } from './track/track.service';
+import { TrackController } from './track/track.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') })],
+  controllers: [AppController, TrackController],
+  providers: [AppService, TrackService],
 })
 export class AppModule {}
